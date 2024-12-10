499e1f4136821669bc350d18fb8f204abd17531a
rohandarade <rohandarade29@gmail.com>
2024-12-11 00:55:38 +0530

    fix: added a check to valid teacher_id and assignment for grading

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -79,7 +79,8 @@ class Assignment(db.Model):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
-
+        assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id,
+                                'You are not authorized to grade this assignment')
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
         db.session.flush()
